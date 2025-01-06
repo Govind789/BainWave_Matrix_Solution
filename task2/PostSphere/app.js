dea8029fd95780128371120ea21299434d55f187
Govind Gupta <gvgupta789@gmail.com>
2025-01-06 12:22:50 +0530

    deploy

--- task2/PostSphere/app.js
+++ task2/PostSphere/app.js
@@ -58,7 +58,7 @@ if(process.env.NODE_ENV === "production"){
     const path = require("path");
 
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,"frontend/build/index.html"));
+        res.sendFile(path.resolve(__dirname,"frontend/dist/index.html"));
     });
 }
 
